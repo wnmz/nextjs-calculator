f3a55e4fc5741363d300d5bbf4812ac177034c3d
unknown <damnwnm@gmail.com>
2024-09-01 18:13:19 +0700

    Create /api/user/me endpoint

--- src/app/api/user/me/route.ts
+++ src/app/api/user/me/route.ts
@@ -1,8 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
+import { errorRespose, successResponse } from "../../responses/baseResponse";
+import { verifyToken } from "@/utils/jwt";
+import prisma from "@/utils/prismaClient";
 
 export async function GET(req: NextRequest) {
-    let token = req.cookies.get("token");
-    let auth = req.headers.get("authorization");
-    console.log(token, auth)
-    return NextResponse.json({ message: "Hello, world! This is a GET request." });
+    let token = req.cookies.get("token")?.value;
+
+    // TODO: Make token validation as another function or create JWT class
+    if(!token) {
+        return NextResponse.json(errorRespose("Unauthorized!"), {status: 500});
+    }
+
+    let jwt = verifyToken(token);
+    if(!jwt.valid || !jwt.payload?.id) {
+        return NextResponse.json(errorRespose("Unauthorized!"), {status: 500});
+    }
+
+    let user = await prisma.user.findUnique({
+        where: {
+            id: jwt.payload.id
+        },
+        include: {
+            calculations: true,
+            memory: true,
+        }
+    });
+
+    if(!user) {
+        return NextResponse.json(errorRespose("Unauthorized!"), {status: 500});
+    }
+
+    return NextResponse.json(successResponse(user));
 }
